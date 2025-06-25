ea33d7a86e77d0c1b34782a820c4acfa1f2085e1
Srijathaduri <srijaveeragoud10@gmail.com>
2025-06-25 16:04:42 +0530

    Updated project code2

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connect = mongoose.connect("mongodb://localhost:27017/Login");
+const connect = mongoose.connect(process.env.MONGO_URI);
 
 // Check database connected or not
 connect.then(() => {
